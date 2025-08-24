d00e998d7aa691da44fb4eb130fee105761843a5
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-08-25 02:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	 event.preventDefault();
+	
+	let radius = Number(document.querySelector("#radius").value);
+
+	if(radius < 0 || !radius){
+		document.querySelector("#volume").value = "NaN";
+	}else{
+		let volume = 4/3 * 3.14 * radius *radius*radius;
+	    document.querySelector("#volume").value = volume.toFixed(4);
+	}
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
